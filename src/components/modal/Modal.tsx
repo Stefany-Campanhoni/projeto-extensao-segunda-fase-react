f1689524cf29c1fcb38fe50fd0e10f66a5d5184d
Stefany Campanhoni <scampanhoni@gmail.com>
2025-04-24 21:55:14 -0300

    create base modal component

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,3 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faX } from "@fortawesome/free-solid-svg-icons"
+import "./modal.css"
+import { Button } from "@components/button/Button"
+
 export function Modal() {
-  return <div>boooo!</div>
+  return (
+    <div className="modal-container">
+      <section className="modal-content">
+        <FontAwesomeIcon
+          icon={faX}
+          width={30}
+          className="close-modal-button"
+        />
+        <section className="confirmation-container">
+          <p>Você deseja mesmo deletar este mentor?</p>
+          <section className="confirmation-buttons-container">
+            <Button variant="danger">Excluir</Button>
+            <Button variant="edit">Cancelar</Button>
+          </section>
+        </section>
+      </section>
+    </div>
+  )
 }
